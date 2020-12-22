e82a01cf2d0ed7c6240ee35e3b7d97233804243b
Marton Lederer <marton@lederer.hu>
2020-12-22 12:07:19 +0100

    chore: remove opengraph component

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,5 @@ export { default as VertoProvider } from "./components/Provider";
 export { useTheme } from "./components/Provider/Theme";
 export { default as Button } from "./components/Button";
 export { default as Page } from "./components/Page";
-export { default as OpenGraph } from "./components/OpenGraph";
 export { default as Card } from "./components/Card";
 export * as Loading from "./components/Loading";
