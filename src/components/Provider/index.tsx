5133d2f35397d5dd4795d4eb6554f88689637859
Marton Lederer <marton@lederer.hu>
2021-03-25 15:22:02 +0100

    chore: refactor wip

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -1,21 +1,37 @@
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { DisplayTheme } from "../../types";
 import { ThemeContext } from "./Theme";
+import { createGlobalStyle } from "styled-components";
 
 export default function Provider({
   theme = "Light",
   children
 }: PropsWithChildren<ProviderProps>) {
-  useEffect(() => {
-    if (theme === "Dark")
-      document.querySelector("html").classList.add("dark-theme");
-    else document.querySelector("html").classList.remove("dark-theme");
+  const [GlobalVariables, setGlobalVariables] = useState(createGlobalStyle`
+    --background-color: #fff;
+    --foreground-color: #000;
+    --light-text: #666;
+    --cec: #CECECE;
+    --skeleton: #E4E4E4;
+    --modal-layer-dark: rgba(0, 0, 0, .8);
+    --success: #00D46E;
+    --warning: #FFD336;
+    --error: #FF0000;
+    
+    --standard-shadow: 0px 10px 20px rgba(0, 0, 0, .2);
+    --darker-shadow: 0px 10px 35px rgba(0, 0, 0, .35);
+    --lighter-shadow: 0px 10px 25px rgba(0, 0, 0, .1);
+  `);
 
+  useEffect(() => {
     document.body.style.backgroundColor = theme === "Light" ? "#fff" : "#000";
   }, [theme]);
 
   return (
-    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={theme}>
+      <GlobalVariables />
+      {children}
+    </ThemeContext.Provider>
   );
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
 /// <reference types="react-scripts" />
 
-import "@geist-ui/style";
 import "./styles/global.sass";
 
 export { default as VertoProvider } from "./components/Provider";
 export { useTheme } from "./components/Provider/Theme";
-export { default as Button } from "./components/Button";
-export { default as Page } from "./components/Page";
-export { default as Card } from "./components/Card";
-export * as Loading from "./components/Loading";
-export { default as Select, useSelect } from "./components/Select";
-export { default as Note } from "./components/Note";
-export { default as Input, useInput } from "./components/Input";
-export { default as Table } from "./components/Table";
-export { default as Modal, useModal } from "./components/Modal";
