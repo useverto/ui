0a558f2f572bdd7f7a0df206e6ad6f4561b70132
Marton Lederer <marton@lederer.hu>
2021-04-20 21:29:42 +0200

    feat: page load animation

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,5 +1,6 @@
-import { CSSProperties, PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import { DefaultProps } from "../../utils";
+import { motion } from "framer-motion";
 import styles from "./Page.module.sass";
 
 export default function Page({
@@ -10,15 +11,18 @@ export default function Page({
   ...props
 }: PropsWithChildren<Props>) {
   return (
-    <div
+    <motion.div
       className={[styles.Page, styles[`Size_${size}`], className ?? ""]
         .filter((val) => val !== "")
         .join(" ")}
       style={style}
       {...props}
+      initial={{ opacity: 0, translateY: 20 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
     >
       {children}
-    </div>
+    </motion.div>
   );
 }
 
