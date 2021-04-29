7edb054565e1240871e5bb210c4c123e6d4432d0
Marton Lederer <marton@lederer.hu>
2021-04-29 10:27:42 +0200

    chore: remove grid

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -17,7 +17,7 @@ export default function Page({
         .join(" ")}
       style={style}
       {...props}
-      initial={{ opacity: 0, translateY: 20 }}
+      initial={{ opacity: 0, translateY: 15 }}
       animate={{ opacity: 1, translateY: 0 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
     >
