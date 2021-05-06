d7d561ed675ded1116f198ec01ae8468668d4def
Marton Lederer <marton@lederer.hu>
2021-05-06 23:34:22 +0200

    feat: update trade card link

--- src/components/Card/Trade.tsx
+++ src/components/Card/Trade.tsx
@@ -1,7 +1,7 @@
 import Card, { Props as BaseProps, Status, StatusTypes } from "./index";
 import { formatBalance, formatTime } from "../../utils";
-import { ArrowRightIcon, CloseIcon } from "@iconicicons/react";
-import { useRouter } from "next/router";
+import { ArrowRightIcon, CloseIcon, ShareIcon } from "@iconicicons/react";
+import Link from "next/link";
 import Tooltip from "../Tooltip";
 import styles from "./Card.module.sass";
 
@@ -17,18 +17,13 @@ export default function Trade({
   status,
   orderID
 }: Props) {
-  const router = useRouter();
-
   return (
     <Card
       className={[styles.Item, className ?? ""]
         .filter((val) => val !== "")
         .join(" ")}
       style={style}
-      onClick={(e) => {
-        router.push(`/orbit/order/${orderID}`);
-        if (onClick) onClick(e);
-      }}
+      onClick={onClick}
     >
       <div className={styles.ItemData}>
         <span className={styles.OrderType}>{type}</span>
@@ -76,6 +71,11 @@ export default function Trade({
             </a>
           </Tooltip>
         )}
+        <Link href={`/orbit/order/${orderID}`}>
+          <a className={styles.Icon + " " + styles.SmallerSpace}>
+            <ShareIcon />
+          </a>
+        </Link>
       </div>
     </Card>
   );
