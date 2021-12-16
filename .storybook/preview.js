c1eeae4cc97a0f4ac57424c921baa4e576b10bb8
Marton Lederer <marton@lederer.hu>
2021-12-16 18:11:13 +0100

    feat: use VertoProvider and global theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { VertoProvider } from "../src/index";
+
 export const parameters = {
   options: {
     storySort: (a, b) => {
@@ -11,6 +14,29 @@ export const parameters = {
       return a[1].kind === b[1].kind
         ? 0
         : a[1].id.localeCompare(b[1].id, { numeric: true });
+    }
+  }
+};
+
+// wrap components with the Verto provider
+export const decorators = [
+  (Story, context) => (
+    <VertoProvider theme={context.globals.theme}>
+      <Story />
+    </VertoProvider>
+  )
+];
+
+// for the ui theme
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "Light",
+    toolbar: {
+      icon: "circlehollow",
+      items: ["Light", "Dark"],
+      showName: true
     }
   }
 };
