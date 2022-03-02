4e5f56fc5faeef5d4954d24f3daa16179bd362f6
Marton Lederer <marton@lederer.hu>
2022-03-02 12:43:10 +0100

    feat: add some props to popover story

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -13,18 +13,32 @@ const storyConfig = {
   }
 } as ComponentMeta<typeof Popover>;
 
-const PopoverTemplate = (args) => <Popover {...args} />;
+const PopoverTemplate = (args) => (
+  <Popover
+    {...args}
+    content={
+      <>
+        <h1>Testing</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      </>
+    }
+  >
+    test
+  </Popover>
+);
 
 export const WithClick = PopoverTemplate.bind({});
 
 WithClick.args = {
-  mode: "click"
+  mode: "click",
+  position: "bottom"
 };
 
 export const WithHover = PopoverTemplate.bind({});
 
 WithHover.args = {
-  mode: "hover"
+  mode: "hover",
+  position: "bottom"
 };
 
 export default storyConfig;
